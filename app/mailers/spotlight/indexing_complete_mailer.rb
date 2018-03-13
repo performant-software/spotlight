501d395b9f0e16d52e59c3bbb45b0c131d902cf8
Andrew Sempere <a@feralresearch.org>
2018-03-13 16:33:25 +0100

    Fixes outgoing mail on indexing complete

--- app/mailers/spotlight/indexing_complete_mailer.rb
+++ app/mailers/spotlight/indexing_complete_mailer.rb
@@ -3,10 +3,14 @@ module Spotlight
   # Notify the curator that we're finished processing a
   # batch upload
   class IndexingCompleteMailer < ActionMailer::Base
+	# Include devise mailer helpers so we can send from the same place as user accounts
+	include Devise::Mailers::Helpers
     def documents_indexed(csv_data, exhibit, user)
       @number = csv_data.length
       @exhibit = exhibit
-      mail(to: user.email, subject: 'Document indexing complete')
+      mail(to: user.email,
+		  from: mailer_sender(devise_mapping),
+		  subject: 'Document indexing complete')
     end
   end
 end
